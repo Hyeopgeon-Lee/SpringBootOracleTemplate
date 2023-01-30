6f3bc4388df186f039b846d1c88b9c61d0780daa
Hyeopgeon Lee <39357722+Hyeopgeon-Lee@users.noreply.github.com>
2023-01-30 14:08:57 +0900

    공지사항 전반적인 수정

--- src/main/java/kopo/poly/service/impl/NoticeService.java
+++ src/main/java/kopo/poly/service/impl/NoticeService.java
@@ -3,23 +3,22 @@ package kopo.poly.service.impl;
 import kopo.poly.dto.NoticeDTO;
 import kopo.poly.persistance.mapper.INoticeMapper;
 import kopo.poly.service.INoticeService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Slf4j
-@Service("NoticeService")
+@RequiredArgsConstructor
+@Service
 public class NoticeService implements INoticeService {
 
+    // RequiredArgsConstructor 어노테이션으로 생성자를 자동 생성함
+    // noticeMapper 변수에 이미 메모리에 올라간 INoticeMapper 객체를 넣어줌
+    // 예전에는 autowired 어노테이션를 통해 설정했었지만, 이젠 생성자를 통해 객체 주입함
     private final INoticeMapper noticeMapper;
-
-    @Autowired
-    public NoticeService(INoticeMapper noticeMapper) {
-        this.noticeMapper = noticeMapper;
-    }
 
     @Override
     public List<NoticeDTO> getNoticeList() throws Exception {
@@ -32,25 +31,27 @@ public class NoticeService implements INoticeService {
 
     @Transactional
     @Override
-    public void InsertNoticeInfo(NoticeDTO pDTO) throws Exception {
+    public NoticeDTO getNoticeInfo(NoticeDTO pDTO, boolean type) throws Exception {
 
-        log.info(this.getClass().getName() + ".InsertNoticeInfo start!");
+        log.info(this.getClass().getName() + ".getNoticeInfo start!");
 
-        noticeMapper.InsertNoticeInfo(pDTO);
+        // 상세보기할 때마다, 조회수 증가하기(수정보기는 제외)
+        if (type) {
+            log.info("Update ReadCNT");
+            noticeMapper.updateNoticeReadCnt(pDTO);
+        }
+
+        return noticeMapper.getNoticeInfo(pDTO);
+
     }
 
     @Transactional
     @Override
-    public NoticeDTO getNoticeInfo(NoticeDTO pDTO) throws Exception {
+    public void insertNoticeInfo(NoticeDTO pDTO) throws Exception {
 
-        log.info(this.getClass().getName() + ".getNoticeInfo start!");
+        log.info(this.getClass().getName() + ".InsertNoticeInfo start!");
 
-        // 상세보기 할때마다, 조회수 증가하기
-        log.info("Update ReadCNT");
-        noticeMapper.updateNoticeReadCnt(pDTO);
-
-        return noticeMapper.getNoticeInfo(pDTO);
-
+        noticeMapper.insertNoticeInfo(pDTO);
     }
 
     @Transactional
